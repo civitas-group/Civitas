5c1520b6d6cf0d20aacf56ef37a3d60ade3e2c5d
woshifrank <frankyyj98@gmail.com>
2020-06-08 21:34:26 -0400

    Add password hashing and JWT token for signup

--- server/src/routes/signup.router.js
+++ server/src/routes/signup.router.js
@@ -4,18 +4,61 @@
 const express = require('express');
 const signUpRouter = express.Router();
 const Account = require('../models/account.model'); // account model
+// JWT Authentication
+// After the user creates an account, a JWT token is signed to grant user access.
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 /* Create a regular user account */
 signUpRouter.post("/regular", (req, res, next) => {
     let newAccount = {
         username: req.body.username,
         password: req.body.password,
-		email: req.body.email,
+		    email: req.body.email,
         group_ids: req.body.group_ids,
         is_supervisor: 0,
         managed_group_ids: req.body.managed_group_ids
-      }
-
+      };
+      
+      bcrypt.genSalt(10,(err,salt) => {
+        if(err) throw err;
+        bcrypt.hash(newAccount.password, salt,(err, hash) => {
+          if(err) throw err;
+          newAccount.password = hash;
+          //newAccount.save()
+          Account.create(newAccount, function(err, result){
+            if(err) throw err;
+            // sign the token
+            jwt.sign(
+              // payload info
+              {username:result.id,
+                email:result.email
+              },
+              process.env.JWT_SECRET,
+              // token expires 1 hour
+              {expiresIn:7200},
+              // call-back
+              (err,token) => {
+                if(err){
+                  res.status(400).send({
+                    success: false,
+                    error: err.message
+                  });
+                }
+                else {
+                  res.status(201).send({
+                      token:token,
+                      success: true,
+                      data: result,
+                      message: "Account created successfully"
+                  });
+                }
+              }
+            );  
+         });
+      });
+    }) 
+    /*
     Account.create(newAccount, function(err, result) {
     if(err){
         res.status(400).send({
@@ -29,15 +72,58 @@ signUpRouter.post("/regular", (req, res, next) => {
             message: "Account created successfully"
         });
     }
-  });
+    });
+    */
 });
 
 /* Create an admin user account */
 signUpRouter.post("/admin", (req, res, next) => {
+  bcrypt.genSalt(10,(err,salt) => {
+    if(err) throw err;
+    bcrypt.hash(req.body.password, salt,(err, hash) => {
+      if(err) throw err;
+      Account.create({
+        username: req.body.username,
+        password: hash,
+        email: req.body.email,
+        group_ids: req.body.group_ids,
+        is_supervisor: 1,
+        managed_group_ids: req.body.managed_group_ids
+       }, function(err, result) {
+        jwt.sign(
+          // payload info
+          {username:result.id,
+            email:result.email
+          },
+          process.env.JWT_SECRET,
+          // token expires 1 hour
+          {expiresIn:7200},
+          // call-back
+          (err,token) => {
+            if(err){
+              res.status(400).send({
+                success: false,
+                error: err.message
+              });
+            }
+            else {
+              res.status(201).send({
+                  token:token,
+                  success: true,
+                  data: result,
+                  message: "Account created successfully"
+              });
+            }
+          }
+        );  
+      });
+    });
+  });
+  /*
    Account.create({
     username: req.body.username,
     password: req.body.password,
-	email: req.body.email,
+	  email: req.body.email,
     group_ids: req.body.group_ids,
     is_supervisor: 1,
     managed_group_ids: req.body.managed_group_ids
@@ -55,6 +141,7 @@ signUpRouter.post("/admin", (req, res, next) => {
         });
     } 
   });
+  */
 });
 
 module.exports = signUpRouter;
